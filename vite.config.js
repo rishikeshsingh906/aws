08e9ea0340763d024dbdd3d9e8d28d02d1e471d7
Gaurav Jadhav <gauravjadhav173@gmail.com>
2024-09-04 17:53:41 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Specify the port here
+    port: 5000, // Specify the port here
   },
 })
